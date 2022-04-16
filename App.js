beeec3003c2a48080bb2a5d0f15dbe1b15573ecc
LeeHoSeop <llss2ssll@gmail.com>
2022-04-16 18:02:58 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,54 @@
+import React, {useState, useEffect, useCallback} from 'react';
+import {SafeAreaView, FlatList, useColorScheme} from 'react-native';
+import CommentItem from './src/CommentItem';
+const comments = [
+  {title: 'comments1', content: 'message1', likes: 1},
+  {title: 'comments2', content: 'message2', likes: 1},
+  {title: 'comments3', content: 'message3', likes: 1},
+  {title: 'comments4', content: 'message4', likes: 1},
+];
+const App = () => {
+  const [commentsList, setCommentsList] = useState(comments);
+  //렌더링 확인
+  // const handleChange = () => {
+  //   console.log('눌림');
+  // };
+  //콜백사용후
+  const handleChange = useCallback(() => {
+    console.log('눌림');
+  }, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCommentsList(pre => [
+        ...pre,
+        {
+          title: `comments${pre.length + 1}`,
+          content: `message${pre.length + 1}`,
+          likes: 1,
+        },
+      ]);
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={commentsList}
+        renderItem={({item, index}) => (
+          <CommentItem
+            item={item}
+            likes={item.likes}
+            index={index}
+            // onClick={handleChange}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ src/CommentItem.js
@@ -0,0 +1,57 @@
+import React, {memo, Profiler, useState, useMemo} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+function CommentItem({item, likes, onClick = () => {}}) {
+  // const [clickCount, setClickCount] = useState(0);
+  const onRenderCallback = (
+    id, // the "id" prop of the Profiler tree that has just committed
+    phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
+    actualDuration, // time spent rendering the committed update
+    baseDuration, // estimated time to render the entire subtree without memoization
+    startTime, // when React began rendering this update
+    commitTime, // when React committed this update
+    interactions, // the Set of interactions belonging to this update
+  ) => {
+    console.log(`actualDuration ${item.title} : ${actualDuration}`);
+    // Aggregate or log render timings...
+  };
+  //프롭스가 아닐때 이벤트 확인
+  const handleClick = () => {
+    // onClick();
+    // setClickCount(pre => pre + 1);
+    alert(`${item.title}눌림`);
+  };
+
+  //클릭이벤트후 다시 그려지는지 확인
+  // const rate = () => {
+  //   console.log('rate check', item.title);
+  //   return item.likes > 10 ? 'Good' : 'Bed';
+  // };
+
+  // const rate = useMemo(() => {
+  //   console.log('rate check', item.title);
+  //   return likes > 10 ? 'Good' : 'Bed';
+  // }, [likes]);
+
+  return (
+    <Profiler id="CommentItem" onRender={onRenderCallback}>
+      <TouchableOpacity
+        style={{
+          borderBottomWidth: 1,
+          borderBottomColor: 'gray',
+          padding: 10,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        onPress={handleClick}>
+        <Text>{item.title}</Text>
+        <Text>{item.content}</Text>
+        <Text>{likes}</Text>
+        {/* <Text>{rate()}</Text> */}
+        {/* <Text>{rate}</Text>
+        <Text>{clickCount}</Text> */}
+      </TouchableOpacity>
+    </Profiler>
+  );
+}
+export default memo(CommentItem);
+// export default CommentItem;
